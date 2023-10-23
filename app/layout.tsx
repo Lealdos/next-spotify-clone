e8522dce04f3d7aff4e5db558abc55ac69969750
Jose Delgado <j3leonardodelgado@gmail.com>
2023-10-22 22:52:47 -0400

    auth and log in log out ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,35 @@
-import { Sidebar } from "@/components/SideBard";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Figtree } from "next/font/google";
-import { SupabaseProvider } from "@/providers/SupabaseProvider";
+import { Sidebar } from '@/components/SideBard';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+import { SupabaseProvider } from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/userProvider';
+import { ModalProvider } from '@/providers/ModalProvidar';
+import { ToasterProvider } from '@/providers/TosterProvider';
 
-const font = Figtree({ subsets: ["latin"] });
+const font = Figtree({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Spotify clone",
-  description: "App listen music",
+    title: 'Spotify clone',
+    description: 'App listen music',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <SupabaseProvider>
-        <Sidebar>{children}</Sidebar>
-        </SupabaseProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={font.className}>
+                <ToasterProvider />
+                <SupabaseProvider>
+                    <UserProvider>
+                        <ModalProvider />
+                        <Sidebar>{children}</Sidebar>
+                    </UserProvider>
+                </SupabaseProvider>
+            </body>
+        </html>
+    );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,75 +1,122 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { twMerge } from "tailwind-merge";
-import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
-import { HiHome } from "react-icons/hi";
-import { BiSearch } from "react-icons/bi";
-import { Button } from "./Buttom";
+'use client';
+import { useRouter } from 'next/navigation';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { twMerge } from 'tailwind-merge';
+import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { HiHome, HiUser } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+import { Button } from './Button';
+import { useAuthModal } from '@/hooks/useAuthModal';
+import { useUser } from '@/hooks/useUsers';
+import toast from 'react-hot-toast/headless';
 
 interface headerProps {
-  children: React.ReactNode;
-  className?: string;
+    children: React.ReactNode;
+    className?: string;
 }
 const Header: React.FC<headerProps> = ({ children, className }) => {
-  const router = useRouter();
-  const handlerLogOut = () => {
-    //
-  };
-  return (
-    <header
-      className={twMerge(
-        `h-fit bg-gradient-to-b from-emerald-700 p-6`,
-        className
-      )}
-    >
-      <div className="w-full mb-4 flex items-center justify-between">
-        <div className="hidden md:flex gap-x-2 items-center">
-          <button
-            className="rounded-full bg-black flex items-center justify-center hover:opacity-80 transition"
-            onClick={() => router.back()}
-          >
-            <RxCaretLeft size={32} className=" text-white" />
-          </button>
-          <button
-            className="rounded-full bg-black flex items-center justify-center hover:opacity-80 transition"
-            onClick={() => router.forward}
-          >
-            <RxCaretRight size={32} className=" text-white" />
-          </button>
-        </div>
-        <div className="flex md:hidden gap-x-2 items-center">
-            <button className="flex items-center justify-center bg-white rounded-full
-            p-2 hover:opacity-80 transition">
-                <HiHome size ={18} className="text-black"/>
-                
-            </button>
-            <button className="flex items-center justify-center bg-white rounded-full
-            p-2 hover:opacity-80 transition">
-                <BiSearch size ={18} className="text-black"/>
-            </button>
-        </div>
-        <div className=" flex justify-between items-center gap-x-4">
-            <>
-            <div>
-                <Button onClick={()=> {} } className="bg-transparent text-neutral-300 font-medium">
-                    sing up
-                </Button>
+    const authModal = useAuthModal();
+    const router = useRouter();
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
+    const handlerLogOut = async () => {
+        const error = await supabaseClient.auth.signOut();
+        //reset song
+        router.refresh();
+
+        if (error) {
+            toast.error(`${error}`);
+        } else {
+            toast.success('Logged out');
+        }
+    };
+    return (
+        <header
+            className={twMerge(
+                `h-fit bg-gradient-to-b from-emerald-700 p-6`,
+                className
+            )}
+        >
+            <div className='w-full mb-4 flex items-center justify-between'>
+                <div className='hidden md:flex gap-x-2 items-center'>
+                    <button
+                        className='rounded-full bg-black flex items-center justify-center hover:opacity-80 transition'
+                        onClick={() => router.back()}
+                    >
+                        <RxCaretLeft size={32} className=' text-white' />
+                    </button>
+                    <button
+                        className='rounded-full bg-black flex items-center justify-center hover:opacity-80 transition'
+                        onClick={() => router.forward}
+                    >
+                        <RxCaretRight size={32} className=' text-white' />
+                    </button>
+                </div>
+                <div className='flex md:hidden gap-x-2 items-center'>
+                    <button
+                        className='flex items-center justify-center 
+                                bg-white rounded-full
+                                  p-2 hover:opacity-80 transition'
+                    >
+                        <HiHome size={18} className='text-black' />
+                    </button>
+                    <button
+                        className='flex items-center justify-center bg-white rounded-full
+            p-2 hover:opacity-80 transition'
+                    >
+                        <BiSearch size={18} className='text-black' />
+                    </button>
+                </div>
+                <div
+                    className='flex justify-between 
+                                items-center gap-x-4'
+                >
+                    {user ? (
+                        <div className='flex gap-x-4 items-center'>
+                            {user.email}
+                            <Button
+                                onClick={handlerLogOut}
+                                className='bg-white px-4 py-2'
+                            >
+                                Logout
+                            </Button>
+                            <Button
+                                onClick={() => router.push('/account')}
+                                className='bg-white'
+                            >
+                                <HiUser />
+                            </Button>
+                        </div>
+                    ) : (
+                        <>
+                            <div>
+                                <Button
+                                    onClick={authModal.onOpen}
+                                    className='bg-transparent
+                                 text-neutral-300 font-medium'
+                                >
+                                    sing up
+                                </Button>
+                            </div>
+                            <div>
+                                <Button
+                                    onClick={authModal.onOpen}
+                                    className='bg-white
+                                text-neutral-800 
+                                px-6
+                                py-2
+                                font-medium'
+                                >
+                                    log in
+                                </Button>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
-            <div>
-                <Button onClick={()=> {} } className="bg-white
-                 text-neutral-800 
-                 px-6
-                 py-2
-                 font-medium">
-                    log in
-                </Button>
-            </div>
-            </>
-        </div>
-      </div>
-        {children}
-    </header>
-  );
+            {children}
+        </header>
+    );
 };
 
 export { Header };
